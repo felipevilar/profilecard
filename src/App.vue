6939803b426c68c69f0348c4cc7079bf9c165fac
felipevilar <felipvilar@gmail.com>
2021-06-14 12:56:16 -0400

    Profile Card

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex w-screen h-screen justify-center items-center p-5">
+    <BaseCard />
+  </div>
+</template>
+
+<script setup>
+import BaseCard from "./components/BaseCard.vue";
+</script>
+
+<style>
+
+</style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col card bg-white rounded-2xl shadow-2xl">
+    <div class="card-header bg-cover rounded-t-2xl justify-center items-end">
+        <div class="card-photo rounded-full border-4 border-white bg-center bg-no-repeat"></div>
+    </div>
+    <div class="flex flex-col flex-grow mt-20 items-center">
+      <div class="flex flex-row">
+        <h1 class="mr-3">Victor Crest</h1>
+        <p>26</p>
+      </div>
+      <p class="text-base mt-1 mb-5">London</p>
+      <div class="h-1 w-full bg-gray-100 mb-5"></div>
+      <div class="flex flex-row card-footer w-full h-full justify-evenly">
+        <div class="flex flex-col items-center">
+          <h1>80K</h1>
+          <p>Followers</p>
+        </div>
+        <div class="flex flex-col items-center">
+          <h1>803K</h1>
+          <p>Likes</p>
+        </div>
+        <div class="flex flex-col items-center">
+          <h1>1.4K</h1>
+          <p>Photos</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
